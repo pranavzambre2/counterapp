a7ec9511aee13dd8468d680611a2f0e559f7c440
pranavzambre2 <99859109+pranavzambre2@users.noreply.github.com>
2023-01-09 17:11:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let counter = document.querySelector('#counter');
+let btns = document.querySelectorAll('.btn');
+let count = 0;
+btns.forEach((btn) => {
+   btn.addEventListener('click', (e) => {
+      const styles = e.currentTarget.classList
+      if (styles.contains('increase')) {
+         count++;
+      } else if (styles.contains('decrease')) {
+         count--;
+      }
+      else {
+         count = 0;
+      }
+      counter.textContent = count;
+
+   })
+})
